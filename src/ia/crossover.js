0eacb5fadc5f8d8cb7ecc3bd86668c2c96b67d9c
Olger19 <goldsag43@gmail.com>
2026-05-20 22:07:50 -0500

    refactor(crossover): actualizar comentario para clarificar la función de crossover

--- src/ia/crossover.js
+++ src/ia/crossover.js
@@ -1,4 +1,4 @@
-// Uniform
+// Crossover: Funcion que combina dos padres para crear un hijo
 export function crossover(parentA, parentB) {
   const child = parentA.map((gene, index) =>
     Math.random() < 0.5 ? gene : parentB[index]
